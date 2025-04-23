e61a19fbd439cb348427f52ac3cf96a87c576f7f
Gerson Estrada <gdestrad@usc.edu>
2025-04-22 17:28:15 -0700

    ssr: false

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 import Image from "next/image";
-import MapComponent from "@/components/map-component" 
+// import MapComponent from "@/components/map-component" 
+
+import dynamic from "next/dynamic";
+
+const MapComponent = dynamic(
+    () => import("@/components/map-component"),
+    { ssr: false, loading: () => <p>Loading map…</p> }
+  );
 
 export default function Home() {
   return (
