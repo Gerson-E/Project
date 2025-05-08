40647339a51fb16fb435954157f730ad7b2c606a
Noura Alyabis <alyabis@usc.edu>
2025-05-08 15:28:44 -0700

    added stuff

--- laptop_subscriber.js
+++ laptop_subscriber.js
@@ -1,5 +1,5 @@
 /*
-Laptop MQTT Subscriber 
+Laptop MQTT Subscriber (runs on laptop)
 
 subscribes to the same topic as the raspi : gps/tracker
 parses incoming JSON messages from RPi (18 features + location)
@@ -8,22 +8,16 @@ prints location + predicted stress
 
 notes:
 - run npm install mqtt
-- ensure mosquitto is running locally (sudo systemctl start mo)
-
-how to run this on laptop:
-
-- run mosquitto broker in terminal (command prompt of laptop): 
-"C:\Program Files\mosquitto\mosquitto.exe" -c "C:\Program Files\mosquitto\mosquitto.conf" -v
-in order to allow permissions as well
-
+- run mosquitto locally on cmd prompt: "C:\Program Files\mosquitto\mosquitto.exe" -c "C:\Program Files\mosquitto\mosquitto.conf" -v
+this is to allow permissions as well
 - then run in seperate terminal: "node laptop_subscriber.js" from the Project file
-
 - on publisher on rpi: change broker_ip to laptop ip + run "python3 raspi_publisher.py"
 */
 
 const mqtt = require('mqtt');
 const { spawn } = require('child_process');
 
+// connect to MQTT broker on laptop
 const brokerUrl = 'mqtt://172.20.10.11:1883';
 const topic = 'gps/tracker';
 
@@ -44,6 +38,7 @@ client.on('message', (topic, message) => {
     try {
         const data = JSON.parse(message.toString());
 
+        // features to be extracted to send to python ML model
         const features = [
             data.EDAR_Mean, data.EDAR_Min, data.EDAR_Max, data.EDAR_Std,
             data.EDAR_Kurtosis, data.EDAR_Skew, data.Num_PeaksR, data.EDAR_Amphitude,
@@ -52,24 +47,29 @@ client.on('message', (topic, message) => {
             data.TEMPR_Max, data.TEMPR_Std
         ];
 
+        // make sure all values are members
         if (features.some(v => typeof v !== 'number' || isNaN(v))) {
             console.error("Invalid feature set received:", features);
             return;
         }
 
-        const py = spawn('python', ['predict.py', ...features.map(String)]);
-        let result = '';
 
+        // spawn child python process to run predict.py with the features
+        const py = spawn('python', ['predict.py', ...features.map(String)]);
+
+        // listens for output from python script and stores results (0, 1, 2)       let result = '';
         py.stdout.on('data', (chunk) => {
             result += chunk.toString();
         });
 
+        // handles errors
         py.stderr.on('data', (chunk) => {
             console.error('Prediction error:', chunk.toString());
         });
 
+        // print prediction result
         py.on('close', () => {
-            const pred = parseInt(result.trim());
+            const pred = parseInt(result.trim()); // get int prediction from Python 
             const stressMap = {
                 0: "No Stress",
                 1: "Medium Stress",
